ec3d28e1a9f1d25051c16ad573b1e20a020889ef
nurmarief <ariefmuhammad897@gmail.com>
2024-02-01 15:07:26 +0700

    create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,12 @@
+const About = () => {
+  return (
+    <div className='mx-auto p-3 max-w-6xl space-y-4'>
+      <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus quidem maxime laboriosam odio autem delectus incidunt officiis sed consequuntur vitae quod, eaque dolorum natus. Aut consequatur laboriosam tempora ea nisi, ut obcaecati quo quaerat molestiae accusantium sapiente. Repellat quisquam quo temporibus molestiae laudantium fuga expedita est nemo? Dicta, hic soluta!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus quidem maxime laboriosam odio autem delectus incidunt officiis sed consequuntur vitae quod, eaque dolorum natus. Aut consequatur laboriosam tempora ea nisi, ut obcaecati quo quaerat molestiae accusantium sapiente. Repellat quisquam quo temporibus molestiae laudantium fuga expedita est nemo? Dicta, hic soluta!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus quidem maxime laboriosam odio autem delectus incidunt officiis sed consequuntur vitae quod, eaque dolorum natus. Aut consequatur laboriosam tempora ea nisi, ut obcaecati quo quaerat molestiae accusantium sapiente. Repellat quisquam quo temporibus molestiae laudantium fuga expedita est nemo? Dicta, hic soluta!</p>
+    </div>
+  )
+}
+
+export default About
